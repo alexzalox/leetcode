963af5439b680f1440aba452ee96440fa7d8a71b
Alex Chen <alex.chen@rockmobile.com>
2024-01-05 17:08:37 +0800

    add .gitignore

--- MySQL/find-customer-referee.sql
+++ MySQL/find-customer-referee.sql
@@ -7,3 +7,8 @@ where referee_id != 2 is not false;
 -- from Customer
 -- where referee_id != 2
 --     or referee_id is null;
+
+-- > select 1 = 1 as A, 1 = NULL as B, NULL = NULL as C, NULL is NULL as D;
+
+-- A       B       C       D
+-- 1       NULL    NULL    1
